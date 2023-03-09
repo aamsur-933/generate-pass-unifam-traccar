a04657bf6f164ec8c319daf17c1956714615738e
Aam Surganda <aam.surganda@unitedfamilyfood.com>
2023-03-09 18:33:32 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Generate Pass Unifam Traccar
+## How to modify
+- modify main.go
+- run '''go run main.go'''
+- run '''go build main.go'''
+
+## How to run
+- '''./main'''
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
+
+func main() {
+	// using time.Now() function
+	// to get the current time
+	currentTime := time.Now()
+
+        // random logic
+        // currentTime = currentTime.AddDate(0, 0, 99)
+
+	r := GetMD5Hash(currentTime.Format("20170709"))
+	fmt.Println(r[0:9])
+}
+
+func GetMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
+}
