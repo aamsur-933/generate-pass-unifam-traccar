d3ff9a0e5f90671b4f0c0d4ff5eba49b20e24aa6
Aam Surganda <aam.surganda@unitedfamilyfood.com>
2023-03-12 20:36:05 +0700

    feat: add optional date parameter

--- main.go
+++ main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"time"
 )
 
 func main() {
-	// using time.Now() function
-	// to get the current time
-	currentTime := time.Now()
+	paramDateRaw := flag.String("date", "", "a string for description")
+	flag.Parse()
 
-        // random logic
-        // currentTime = currentTime.AddDate(0, 0, 99)
+	paramDate := ""
+	if *paramDateRaw == "" {
+		// using time.Now() function
+		// to get the current time
+		currentTime := time.Now()
+		paramDate = currentTime.Format("20060102")
+	} else {
+		paramDate = *paramDateRaw
+	}
 
-	r := GetMD5Hash(currentTime.Format("20060102"))
+	// random logic
+	// currentTime = currentTime.AddDate(0, 0, 99)
+
+	r := GetMD5Hash(paramDate)
 	fmt.Printf("Password : %s \n", r[0:10])
 }
 
